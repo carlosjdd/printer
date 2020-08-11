7d3c7c575cff9222e128ebfeea26ddc7d0a591ce
carlosjdd <carlosjdd@users.noreply.github.com>
2020-08-11 12:02:36 +0200

    Include callback for rgb lightning

--- src/on_off.py
+++ src/on_off.py
@@ -2,19 +2,31 @@
 
 import rospy
 from std_msgs.msg import Bool
+from std_msgs.msg import Int16MultiArray
 import RPi.GPIO as GPIO
 
-def callback(data):
+def callback_on(data):
     rec = data.data
     if rec == True:
 		GPIO.output(18,GPIO.HIGH)
     elif rec == False:
 		GPIO.output(18,GPIO.LOW)
 
+def callback_rgb(data):
+    rec = data.data
+    red = rec[0]
+    green = rec[1]
+    blue = rec [2]
+
+    print("Red: " + red)
+    print("Green: " + green)
+    print("Blue: " + blue)
+
 def listener():
 
     rospy.init_node('on_off_listener', anonymous=True)
-    rospy.Subscriber("switch_printer", Bool, callback)
+    rospy.Subscriber("switch_printer", Bool, callback_on)
+    rospy.Subscriber("switch_rgb", Int16MultiArray, callback_rgb)
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
